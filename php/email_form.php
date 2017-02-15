52c45504484d5854255cc2058339353715399a5f
saikovuri <saikovuri@gmail.com>
2017-02-14 22:05:04 -0600

    Revamped whole website

---
+++ php/email_form.php
@@ -0,0 +1,146 @@
+<?php
+ 
+if(isset($_POST['email'])) {
+ 
+    
+ 
+    $email_to = "saikovuri367@gmail.com";
+ 
+    $email_subject = "Email from website";
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "I am very sorry, but there were error(s) found with the form you submitted. ";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Please go back and fix these errors.<br /><br />";
+ 
+        die();
+ 
+    }
+ 
+     
+ 
+    // validation expected data exists
+ 
+    if(!isset($_POST['contactName']) ||
+ 
+        !isset($_POST['contactEmail']) ||
+ 
+        !isset($_POST['contactSubject']) ||
+ 
+         !isset($_POST['contactMessage']) 
+ 
+        // || !isset($_POST['comments'])) 
+
+        {
+        died('I am sorry, but there appears to be a problem with the form you submitted.');       
+ 
+    }
+ 
+     
+ 
+    $first_name = $_POST['contactName']; // required
+ 
+    $email_from = $_POST['contactEmail']; // required
+ 
+    $emailsubject = $_POST['contactSubject']; // required
+ 
+    $comments = $_POST['contactMessage']; // not required
+ 
+    //$comments = $_POST['comments']; // required
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$first_name)) {
+ 
+    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+  if(!preg_match($string_exp,$emailsubject)) {
+ 
+    $error_message .= 'Please mention the subject line.<br />';
+ 
+  }
+ 
+  if(strlen($comments) < 2) {
+ 
+    $error_message .= 'The message you entered do not appear to be valid.<br />';
+ 
+  }
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+ 
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+ 
+    $email_message .= "Subject: ".clean_string($emailsubject)."\n";
+ 
+    $email_message .= "Message: ".clean_string($comments)."\n";
+ 
+     
+ 
+     
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+ 
+<!-- include your own success html here -->
+ 
+ 
+ 
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+ 
+ 
+<?php
+ 
+}
+ 
+?>
